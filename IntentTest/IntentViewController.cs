b18df5fd991dbfd51565aeddd36c8946de4eb842
adamped <adam.pedley@gmail.com>
2016-08-09 16:32:30 +1000

    Initial check-in of module SiriKitTest

---
+++ IntentTest/IntentViewController.cs
@@ -0,0 +1,15 @@
+﻿using UIKit;
+using IntentsUI;
+using CoreGraphics;
+using Intents;
+using ObjCRuntime;
+using System;
+
+class IntentViewController : UIViewController, IINUIHostedViewControlling
+{
+
+	public void Configure(INInteraction interaction, INUIHostedViewContext context, Action<CGSize> completion)
+	{
+		throw new NotImplementedException();
+	}
+}
