5c6591a378e53cb171826b83da039092b078086c
adamped <adam.pedley@gmail.com>
2016-08-10 12:35:28 +1000

    IntentHandle Update

--- IntentTest/IntentHandle.cs
+++ IntentTest/IntentHandle.cs
@@ -1,10 +1,34 @@
 ﻿using System;
+using Foundation;
+using Intents;
+using ObjCRuntime;
+
 namespace IntentTest
 {
-	public class IntentHandle
+	public class IntentHandle: INExtension, IINSendMessageIntentHandling
 	{
 		public IntentHandle()
 		{
+		}
+
+		public void HandleSendMessage(INSendMessageIntent intent, Action<INSendMessageIntentResponse> completion)
+		{
+			var userActivity = new NSUserActivity(activityType: "INSendMessageIntent");
+
+			var response = new INSendMessageIntentResponse(code: .success, userActivity: userActivity);
+
+			completion(response);
+		}
+		public override ResolveContent(INSendMessageIntent intent, Action<INStringResolutionResult> completion)
+		{
+
+			if (intent.Content != "")
+			{
+				completion(INStringResolutionResult.GetSuccess(intent.Content));
+			}
+			else {
+				completion(INStringResolutionResult.NeedsValue);
+			}
 		}
 	}
 }
